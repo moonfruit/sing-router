80631e6172cb8edbac438ff85ce5b295035f63f9
MoonFruit <dkmoonfruit@gmail.com>
2026-05-05 20:24:50 +0800

    fix(cli): script command paths after firmware/ asset reorg

--- internal/cli/script.go
+++ internal/cli/script.go
@@ -11,18 +11,19 @@ import (
 )
 
 var scriptMap = map[string]string{
-	"startup":        "shell/startup.sh",
-	"teardown":       "shell/teardown.sh",
-	"init.d":         "initd/S99sing-router",
-	"nat-start":      "jffs/nat-start.snippet",
-	"services-start": "jffs/services-start.snippet",
+	"startup":               "shell/startup.sh",
+	"teardown":              "shell/teardown.sh",
+	"init.d":                "initd/S99sing-router",
+	"merlin/nat-start":      "firmware/merlin/nat-start.snippet",
+	"merlin/services-start": "firmware/merlin/services-start.snippet",
+	"koolshare/N99":         "firmware/koolshare/N99sing-router.sh",
 }
 
 func newScriptCmd() *cobra.Command {
 	var remote bool
 	cmd := &cobra.Command{
 		Use:   "script <name>",
-		Short: "Print embedded script (startup|teardown|init.d|nat-start|services-start)",
+		Short: "Print embedded script (startup|teardown|init.d|merlin/nat-start|merlin/services-start|koolshare/N99)",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			name := args[0]
@@ -41,7 +42,7 @@ func newScriptCmd() *cobra.Command {
 			}
 			path, ok := scriptMap[name]
 			if !ok {
-				return fmt.Errorf("unknown script %q (one of: startup, teardown, init.d, nat-start, services-start)", name)
+				return fmt.Errorf("unknown script %q (one of: startup, teardown, init.d, merlin/nat-start, merlin/services-start, koolshare/N99)", name)
 			}
 			data, err := assets.ReadFile(path)
 			if err != nil {
